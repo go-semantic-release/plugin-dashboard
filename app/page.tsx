1309df2f4bb27f299de91fff29253985fe3e8519
Christoph Witzko <github@christophwitzko.com>
2023-10-16 15:46:41 +0200

    feat: revalidate cachge after 5min

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-"use server";
 import { PluginOverview } from "@/app/client";
 import { getAllPlugins } from "@/lib/registry";
 
+export const revalidate = 300;
 export default async function Home() {
   const plugins = await getAllPlugins();
   plugins.sort((a, b) => a.FullName.localeCompare(b.FullName));
